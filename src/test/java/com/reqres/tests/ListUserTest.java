e21040cfaad5ae9f902adc61572fc37b07ec7f35
Suresh Shaw <Suresh.Shaw@hyland.com>
2020-12-28 15:16:19 +0530

    code refactored

--- src/test/java/com/reqres/tests/ListUserTest.java
+++ src/test/java/com/reqres/tests/ListUserTest.java
@@ -3,32 +3,38 @@ package com.reqres.tests;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import com.reqres.response.model.ListUserResponsePojo;
 import com.reqres.uitilites.APIConstants;
 
 import io.restassured.RestAssured;
-import io.restassured.http.Method;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 
 public class ListUserTest extends BaseTest{
+
+	private ListUserResponsePojo listUserResponsePojo;
+	private RequestSpecification request;
 	
-	ListUserResponsePojo listUserResponsePojo;
-	
+	@BeforeClass
+	public void setUpRequestSpec() {
+
+		request = RestAssured.given();
+		request.baseUri(APIConstants.BASEURI);
+		request.basePath("/api/users");
+	}
+
 	@Test
 	public void listUserTest() {
-		RestAssured.baseURI=APIConstants.BASEURI;
-		RestAssured.basePath="/api/users";
-		
-		RequestSpecification request = RestAssured.given();
+
 		Map<String,Object> queryParam = new HashMap<String, Object>();
 		queryParam.put("page", 2);
 		request.queryParams(queryParam);
-		Response response = request.request(Method.GET);
+		Response response = request.get();
 		softAssert.assertEquals(response.statusCode(),200);
-		
+
 		listUserResponsePojo = response.as(ListUserResponsePojo.class);
 		softAssert.assertTrue(listUserResponsePojo.support.text.equals("To keep ReqRes free, contributions towards server costs are appreciated!"));
 		softAssert.assertEquals(listUserResponsePojo.total,12);
--- src/test/java/com/reqres/tests/LoginTests.java
+++ src/test/java/com/reqres/tests/LoginTests.java
@@ -1,8 +1,13 @@
 package com.reqres.tests;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -17,7 +22,6 @@ import com.reqres.uitilites.ReadProperties;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
-import io.restassured.http.Method;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import io.restassured.module.jsv.JsonSchemaValidator;
@@ -25,24 +29,40 @@ import io.restassured.module.jsv.JsonSchemaValidator;
 public class LoginTests extends BaseTest{
 
 	private LoginRequestPojo loginRequestPojo;
+	//private LoginRequestPojo loginRequestPojo1;
 	private Login200ResponsePojo login200ResponsePojo;
 	private Login400ResponsePojo login400ResponsePojo;
+	private RequestSpecification request;
+
+	@BeforeClass
+	public void setUpRequestSpec() {
+
+		request = RestAssured.given();
+		request.baseUri(APIConstants.BASEURI);
+		request.basePath("/api/login");
+	}
 
 	@Test
 	public void loginSuccessTest() {
 
 		loginRequestPojo = new LoginRequestPojo();
-
 		loginRequestPojo.setEmail(ReadProperties.getValue("EMAIL"));
 		loginRequestPojo.setPassword(ReadProperties.getValue("PASSWORD"));
-		
-		RestAssured.baseURI=APIConstants.BASEURI;
-		RestAssured.basePath="/api/login";
 
-		RequestSpecification request = RestAssured.given();
+
+		//		loginRequestPojo1 = new LoginRequestPojo();
+		//		loginRequestPojo1.setEmail(ReadProperties.getValue("EMAIL"));
+		//		loginRequestPojo1.setPassword(ReadProperties.getValue("PASSWORD"));
+
+		//		List<LoginRequestPojo> all = new ArrayList<LoginRequestPojo>();
+		//		all.add(loginRequestPojo);
+		//		all.add(loginRequestPojo1);
+
+		//		request.body(all);
+
 		request.body(loginRequestPojo);
 		request.contentType(ContentType.JSON);
-		Response response = request.request(Method.POST);
+		Response response = request.post();
 
 		softAssert.assertEquals(response.getStatusCode(), 200);
 
@@ -53,38 +73,68 @@ public class LoginTests extends BaseTest{
 
 	@Test
 	public void loginFailureTest() {
+		//		RestAssured
+		//		.given()
+		//		// Logging all details
+		//		.log()
+		//		.all()
+		//		.when()
+		//		.get();
+
 		loginRequestPojo = new LoginRequestPojo();
 		loginRequestPojo.setEmail(ReadProperties.getValue("EMAIL"));
-		
-		RestAssured.baseURI=APIConstants.BASEURI;
-		RestAssured.basePath="/api/login";
 
-		RequestSpecification request = RestAssured.given();
 		request.body(loginRequestPojo);
 		request.contentType(ContentType.JSON);
-		Response response = request.request(Method.POST);
+		Response response = request.post();
 
 		softAssert.assertEquals(response.getStatusCode(), 200);
-		
+
 		login400ResponsePojo = response.as(Login400ResponsePojo.class);
 		softAssert.assertTrue(login400ResponsePojo.error.equals("Missing password"));
-		softAssert.assertNotNull(login400ResponsePojo.error);// checking that error property/key of response json must exists
+		softAssert.assertNotNull(login400ResponsePojo.error);// checking that value of error key/property must exists  
 
-		softAssert.assertTrue(response.then().body(JsonSchemaValidator.matchesJsonSchema(new File("responseJSON/loginUnsucessfull400.json"))).toString().startsWith("io.restassured.internal.ValidatableResponseImpl"));
-				
-		//System.out.println(login400ResponsePojo.error1);
+		//validating response JSON schema so validating that all keys must be present in response json 
+		softAssert.assertTrue(response.then().body(JsonSchemaValidator.matchesJsonSchema(new File("responseJSONSchema/loginUnsucessfull400Schema.json"))).toString().startsWith("io.restassured.internal.ValidatableResponseImpl"));
+
 		testMsg ="Missing password";
+
+		// Another way to validate all the presence of all keys/properties
+		List<String> expectedKey = new ArrayList<String>();
+		expectedKey.add("error");
+
+		List<String> actualKey = new ArrayList<String>();
+
 		ObjectMapper om = new ObjectMapper();
 		try {
 			JsonNode node = om.readTree(response.asString());
 			Iterator<String> allNodes = node.fieldNames();
-			allNodes.forEachRemaining(k -> {
-				System.out.println(k);
-			});
+			while(allNodes.hasNext()) {
+				actualKey.add(allNodes.next());
+			}
 		} catch (JsonMappingException e) {
 			e.printStackTrace();
 		} catch (JsonProcessingException e) {
 			e.printStackTrace();
+		}
+
+		softAssert.assertEqualsNoOrder(expectedKey.toArray(), actualKey.toArray());
+
+		//saving the response json as failure proof as we do in ui automation by taking screenshot of page where bug is found. 
+		//This saved json can be used as request payload for any other API
+		FileWriter writer = null;
+		try {
+			writer = new FileWriter("responseJSON/loginUnsucessfull400.json");
+			writer.write(response.asPrettyString());
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+		finally {
+			try {
+				writer.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
 	}
 
